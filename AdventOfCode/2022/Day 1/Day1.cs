c4836c5888aedf818039d098b069270e993792b1
teppich <johannesspicker@me.com>
2022-12-01 13:36:15 +0100

    Day 1 complete

---
+++ AdventOfCode/2022/Day 1/Day1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode._2022.Tools;
+
+namespace AdventOfCode._2022.Day_1
+{
+    public class Day1
+    {
+        public static int Solution1() => GetElfTotals(GetInput()).Max();
+        public static int Solution2() => GetElfTotals(GetInput()).OrderByDescending(x => x).Take(3).ToArray().Sum();
+
+        public static void PrintSolution1() => Console.WriteLine(Solution1());
+        public static void PrintSolution2() => Console.WriteLine(Solution2());
+
+        private static string[] GetInput() =>
+            InputReader.ReadInputFileStringArray(@"D:\GitRepos\AdventOfCode\AdventOfCode\2022\Day 1\Input.txt");
+
+        private static List<int> GetElfTotals(string[] calories)
+        {
+            List<int> elfTotals    = new List<int>();
+            int       currentTotal = 0;
+
+            for (int i = 0; i < calories.Length; i++)
+            {
+                string line = calories[i];
+
+                if (line != "")
+                    currentTotal += int.Parse(line);
+                else
+                {
+                    elfTotals.Add(currentTotal);
+                    currentTotal = 0;
+                }
+            }
+
+            return elfTotals;
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AdventOfCode._2022.Day_1;
+
+namespace AdventOfCode
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Day1.PrintSolution1();
+            Day1.PrintSolution2();
+        }
+    }
+}
